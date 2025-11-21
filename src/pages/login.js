353131857894e80c12b1701de48a6310a6df43ad
DevDeived <david.fisica@yahoo.com.br>
2025-11-21 12:46:05 -0300

    fix final: mudando login apra deploy

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import md5 from "md5";
 import api from "../lib/api";
 
 const Container = styled.div`
